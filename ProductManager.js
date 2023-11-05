60e24080fd4c8f45a4cb78a7d326a25ae00b9102
Jesus Enrique Marin Pacheco <jesusmarinp17@gmail.com>
2023-11-05 12:22:21 -0500

    actualizando

--- ProductManager.js
+++ ProductManager.js
@@ -1,60 +1,61 @@
 class ProductManager {
     constructor() {
-        this.products = [];
-        this.id = 0;
+      this.products = [];
+      this.nextId = 1;
     }
-
+  
+    addProduct(title, description, price, thumbnail, code, stock) {
+      if (!title || !description || !price || !thumbnail || !code || stock === undefined) {
+        throw new Error('Todos los campos son obligatorios.');
+      }
+  
+      const isCodeUnique = this.products.every((product) => product.code !== code);
+      if (!isCodeUnique) {
+        throw new Error('El código del producto ya está en uso.');
+      }
+  
+      const newProduct = {
+        id: this.nextId,
+        title,
+        description,
+        price,
+        thumbnail,
+        code,
+        stock,
+      };
+  
+      this.products.push(newProduct);
+      this.nextId++;
+    }
+  
     getProducts() {
-        return this.products;
+      return this.products;
     }
-
-    addProduct(product) {
-        if (this.products.find(p => p.code === product.code)) {
-            throw new Error('El código del producto ya existe');
-        }
-        this.id++;
-        this.products.push({ ...product, id: this.id });
-    }
-
+  
     getProductById(id) {
-        const product = this.products.find(p => p.id === id);
-        if (!product) {
-            throw new Error('Producto no encontrado');
-        }
-        return product;
+      const product = this.products.find((product) => product.id === id);
+      if (!product) {
+        console.error('Not Found');
+      }
+      return product;
     }
-}
-
-const pm = new ProductManager();
-console.log(pm.getProducts()); // []
-
-pm.addProduct({
-    title: 'producto prueba',
-    description: 'Este es un producto prueba',
-    price: 200,
-    thumbnail: 'Sin imagen',
-    code: 'abc123',
-    stock: 25
-});
-
-console.log(pm.getProducts()); // [{ title: 'producto prueba', description: 'Este es un producto prueba', price: 200, thumbnail: 'Sin imagen', code: 'abc123', stock: 25, id: 1 }]
-
-try {
-    pm.addProduct({
-        title: 'producto prueba',
-        description: 'Este es un producto prueba',
-        price: 200,
-        thumbnail: 'Sin imagen',
-        code: 'abc123',
-        stock: 25
-    });
-} catch (e) {
-    console.error(e.message); // El código del producto ya existe
-}
-
-try {
-    console.log(pm.getProductById(1)); // { title: 'producto prueba', description: 'Este es un producto prueba', price: 200, thumbnail: 'Sin imagen', code: 'abc123', stock: 25, id: 1 }
-    console.log(pm.getProductById(2)); // Producto no encontrado
-} catch (e) {
-    console.error(e.message);
-}
+  }
+  
+  const productManager = new ProductManager();
+  
+  try {
+    productManager.addProduct('Producto 1', 'Descripción del Producto 1', 100, 'imagen1.jpg', 'P123', 50);
+    productManager.addProduct('Producto 2', 'Descripción del Producto 2', 150, 'imagen2.jpg', 'P124', 30);
+    productManager.addProduct('Producto 3', 'Descripción del Producto 3', 200, 'imagen3.jpg', 'P123', 20); // Generará un error de código repetido
+  } catch (error) {
+    console.error(error.message);
+  }
+  
+  const allProducts = productManager.getProducts();
+  console.log('Todos los productos:', allProducts);
+  
+  const productById = productManager.getProductById(2);
+  if (productById) {
+    console.log('Producto encontrado por ID:', productById);
+  }
+  
